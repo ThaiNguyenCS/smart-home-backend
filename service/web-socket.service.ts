36772dd8d03c6cb1e43e1aef1c446370b43d6fd8
Thai Nguyen <nh0kcrazy04@gmail.com>
2025-04-14 10:17:25 +0700

    apply SOLID for socket service

--- service/web-socket.service.ts
+++ service/web-socket.service.ts
@@ -18,32 +18,41 @@ export function initWebSocket(server: any) {
     io.on("connection", (socket) => {
         logger.info(`User connected: ${socket.id}`);
         socket.send("Hello from server");
-
-        const jwtToken = socket.handshake.auth.token;
-        logger.info(`JWT token: ${jwtToken}`);
-        if (!jwtToken) {
-            socket.emit("error", "Missing token in socket connection");
-            socket.disconnect(true);
-            return
+        const userId = authenticateSocket(socket);
+        if (!userId) {
+            return;
         }
-        const decoded = verifyToken(jwtToken);
-        if (!decoded) {
-            socket.emit("error", "Invalid token in socket connection");
-            socket.disconnect(true);
-            return
-        }
-
-        const userId = decoded.id;
-        socket.join(userId);
-
-        socket.on("disconnect", () => {
-            logger.info(`User disconnected: socketId ${socket.id}`);
-            socket.leave(userId);
-            socket.disconnect(true);
-        });
+        socket.join(userId); // join room
+        registerDisconnectHandler(socket, userId)
     });
 }
 
+function authenticateSocket(socket: any) {
+    const jwtToken = socket.handshake.auth.token;
+    logger.info(`JWT token: ${jwtToken}`);
+    if (!jwtToken) {
+        socket.emit("error", "Missing token in socket connection");
+        socket.disconnect(true);
+        return null;
+    }
+    const decoded = verifyToken(jwtToken);
+    if (!decoded) {
+        socket.emit("error", "Invalid token in socket connection");
+        socket.disconnect(true);
+        return null;
+    }
+    return decoded.id
+}
+
+
 export function sendWebSocketNotification(userId: string, notification: any) {
     io.to(userId).emit("notification", JSON.stringify(notification));
+}
+
+function registerDisconnectHandler(socket: any, userId: string) {
+    socket.on("disconnect", () => {
+        logger.info(`User disconnected: socketId ${socket.id}`);
+        socket.leave(userId);
+        socket.disconnect(true);
+    });
 }
